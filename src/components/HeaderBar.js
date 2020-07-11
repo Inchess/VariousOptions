7801a93eaaafdaa83ed05ce22b3f62c29dfbdb51
Inchess <tomasz.kubit@performgroup.com>
2020-07-11 21:30:56 +0200

    Initial commit

---
+++ src/components/HeaderBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeaderBar() {
+    return (
+        <div className="headerBar">
+            This is some tecxt;ag
+        </div>
+    )
+}
+
+export default HeaderBar;
